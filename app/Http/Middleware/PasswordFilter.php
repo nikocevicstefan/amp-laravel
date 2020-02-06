6a110825d19ad6f3e916c3b59f9badb1828f578b
nikocevicstefan <nikocevicstefan@gmail.com>
2020-02-06 22:36:31 +0100

    Removes redundant checks in the password filter middleware

--- app/Http/Middleware/PasswordFilter.php
+++ app/Http/Middleware/PasswordFilter.php
@@ -15,12 +15,9 @@ class PasswordFilter
      */
     public function handle($request, Closure $next)
     {
-        if(!empty($request->header('X-Password'))){
-            if($request->header('X-Password') === 'Fortuna Major'){
-                return $next($request);
-            }
-            return response()->json(['message'=>'Incorrect Password | 401'],401);
+        if($request->header('X-Password') === 'Fortuna Major'){
+            return $next($request);
         }
-        return response()->json(['message'=>'Incorrect Password | 401'],401);
+        return response()->json(['message'=>'Incorrect Password'],401);
     }
 }
